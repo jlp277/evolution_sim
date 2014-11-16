d6c0b4fd2b8eacfe23b34ce7aec4c1b49db313a8
James Park <jlp277@cornell.edu>
2014-11-16 16:20:39 -0500

    Fixed issue where organisms were spawning at the same location and not being able to move.

--- environment.py
+++ environment.py
@@ -29,18 +29,34 @@ clock = pygame.time.Clock()
 orgLock = Lock()
 orgCond = Condition(orgLock)
 
+orgSize = 5
+
 class Habitat(Thread):
 	def __init__(self):
 		Thread.__init__(self)
 		# self.organisms = pygame.sprite.Group()
 		self.organisms = []
 		for id in range(20):
+			with orgLock:
+				(x, y) = self.getUnoccupiedSpace()
+				organism = Organism(id, 0, x, y, 10, self)
+				organism.start()
+				# organism.body.add(self.organisms)
+				self.organisms.append(organism)
+
+	def getUnoccupiedSpace(self): # inefficient
+		while True:
 			x = random.randrange(screensize[0])
 			y = random.randrange(screensize[1])
-			organism = Organism(id, 0, x, y, 10, self)
-			organism.start()
-			# organism.body.add(self.organisms)
-			self.organisms.append(organism)
+			tmpBody = OrganismBody(orgSize, orgSize)
+			tmpBody.rect.x = x
+			tmpBody.rect.y = y
+			siamese = False
+			for org in self.organisms:
+				if pygame.sprite.collide_rect(tmpBody, org.body):
+					siamese = True
+			if siamese == False:
+				return (x, y)
 
 	def run(self):
 		while True:
@@ -51,18 +67,17 @@ class Habitat(Thread):
 			pass
 
 class OrganismBody(pygame.sprite.Sprite):
-	def __init__(self, color, width, height):
+	def __init__(self, width, height):
 		super().__init__() # sprite constructor
 		self.image = pygame.Surface([width, height])
-		# self.image.fill(color)
 		self.rect = self.image.get_rect()
 
 class Organism(Thread):
 	def __init__(self, id, generation, initX, initY, maxHealth, habitat):
 		Thread.__init__(self)
 		self.color = BLACK
-		self.sizeX = 10
-		self.sizeY = 10
+		self.sizeX = orgSize
+		self.sizeY = orgSize
 		self.generation = generation
 		self.id = id
 		self.posX = initX
@@ -70,7 +85,7 @@ class Organism(Thread):
 		self.velX = 1
 		self.velY = 1
 		self.habitat = habitat
-		self.body = OrganismBody(self.color, self.sizeX, self.sizeY)
+		self.body = OrganismBody(self.sizeX, self.sizeY)
 		self.health = maxHealth
 		# self.brain = Brain()
 
@@ -98,8 +113,8 @@ class Organism(Thread):
 		self.body.rect.y = self.posY
 		self.body.rect.x = self.posX
 
+		# check for collision(s) with others. inefficient?
 		for org in self.habitat.organisms:
-			# check for collision(s) with others
 			if org == self:
 				continue
 			if pygame.sprite.collide_rect(self.body, org.body):
