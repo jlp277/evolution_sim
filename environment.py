34bf15bcff51b198bd2979bc83353719ef2b3008
James Park <jlp277@cornell.edu>
2014-11-16 15:45:19 -0500

    Added collision detection. For now the org causing the collision moves in away in the opposite direction.

--- environment.py
+++ environment.py
@@ -25,7 +25,7 @@ pygame.init()
 screensize = (700, 500)
 screen = pygame.display.set_mode(screensize)
  
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("evolution")
  
 # Loop until the user clicks the close button.
 done = False
@@ -41,12 +41,14 @@ orgCond = Condition(orgLock)
 class Habitat(Thread):
 	def __init__(self):
 		Thread.__init__(self)
+		# self.organisms = pygame.sprite.Group()
 		self.organisms = []
 		for id in range(20):
 			x = random.randrange(screensize[0])
 			y = random.randrange(screensize[1])
-			organism = Organism(id, 0, x, y)
+			organism = Organism(id, 0, x, y, self)
 			organism.start()
+			# organism.body.add(self.organisms)
 			self.organisms.append(organism)
 
 	def run(self):
@@ -54,22 +56,32 @@ class Habitat(Thread):
 			# grow food and shit
 			pass
 
+class OrganismBody(pygame.sprite.Sprite):
+	def __init__(self, color, width, height):
+		super().__init__() # sprite constructor
+		self.image = pygame.Surface([width, height])
+		# self.image.fill(color)
+		self.rect = self.image.get_rect()
+
 class Organism(Thread):
-	def __init__(self, id, generation, initX, initY):
+	def __init__(self, id, generation, initX, initY, habitat):
 		Thread.__init__(self)
 		self.color = BLACK
-		self.sizeX = 5
-		self.sizeY = 5
+		self.sizeX = 10
+		self.sizeY = 10
 		self.generation = generation
 		self.id = id
 		self.posX = initX
 		self.posY = initY
 		self.velX = 1
 		self.velY = 1
+		self.habitat = habitat
+		self.body = OrganismBody(self.color, self.sizeX, self.sizeY)
 		# self.health = maxHealth
 		# self.brain = Brain()
 
 	def update(self):
+		global orgLock
 		# update health
 		# self.health -= 1
 		# update position
@@ -77,24 +89,45 @@ class Organism(Thread):
 		self.posX += self.velX
 		self.posY += self.velY
 
+		# check collisions
 		if self.posY > screensize[1]:
-		    self.posY = screensize[1]
+			self.posY = screensize[1]
 		elif self.posY < 0:
 			self.posY = 0
 		if self.posX > screensize[0]:
-		    self.posY = screensize[0]
+			self.posY = screensize[0]
 		elif self.posX < 0:
 			self.posX = 0
 
+		# update sprite
+		self.body.rect.y = self.posY
+		self.body.rect.x = self.posX
+
+		for org in self.habitat.organisms:
+			# check for collision(s) with others
+			if org == self:
+				continue
+			if pygame.sprite.collide_rect(self.body, org.body):
+				# collision. move away.
+				self.posX += -1 * self.velX
+				self.posY += -1 * self.velY
+
+		# update sprite
+		self.body.rect.y = self.posY
+		self.body.rect.x = self.posX
+
 		# random directions
-		self.velX = random.choice([-1,1]) * random.randrange(2)
-		self.velY = random.choice([-1,1]) * random.randrange(2)
+		self.velX = random.choice([-1,1]) * random.randrange(3)
+		self.velY = random.choice([-1,1]) * random.randrange(3)
+
+		return True
 
 	def run(self):
 		while True:
 			time.sleep(.01)
 			with orgLock:
-				self.update()
+				if not self.update():
+					break # death
 
 habitat = Habitat()
 habitat.start()
@@ -118,7 +151,7 @@ while not done:
 
 	for org in habitat.organisms:
 		with orgLock:
-			pygame.draw.rect(screen, BLACK, [org.posX, org.posY, org.sizeX, org.sizeY])
+			pygame.draw.rect(screen, org.color, [org.posX, org.posY, org.sizeX, org.sizeY])
  
 	# --- Go ahead and update the screen with what we've drawn.
 	pygame.display.flip()
